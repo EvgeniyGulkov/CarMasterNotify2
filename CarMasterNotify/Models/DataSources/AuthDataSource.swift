bcfd74b3390ffbd3706c1e0e7a04444e982cc534
Evgeniy <ewgeniy2004@list.ru>
2020-05-23 21:02:23 +0700

    change static table to dinamic

---
+++ CarMasterNotify/Models/DataSources/AuthDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  AuthDataSource.swift
+//  CarMasterNotify
+//
+//  Created by Gulkov on 23.05.2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import Foundation
---
+++ CarMasterNotify/Views/TableViewCells/ButtonTableViewCell/ButtonTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  ButtonTableViewCell.swift
+//  CarMasterNotify
+//
+//  Created by Gulkov on 23.05.2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import Foundation
